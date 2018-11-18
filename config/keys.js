8b53e56a9a17e24aa526f2ccaa7c8b2d41723e61
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2018-11-18 16:40:37 -0300

    set herokuapp

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    appKey : 'your-custom-key',
-    googleClientID:'your client id',
-    googleClientSecret:'your client secret'
+    appKey : '6v9y$B?E(H+MbQeThWmZq4t7w!z%C*F)p2s5v8y/B?D(G+KbPeShVmYq3t6w9z$CUjXn2r5u8x/A%D*G-KaPdSgVkYp3s6v9cQfTjWnZr4u7x!A%C*F-JaNdRgUkXp2sH+MbQeThWmZq4t7w!z%C&F)J@NcRfUjX',
+    googleClientID:'510296068668-g2944itpetoukftit64nbhdd60l24l6b.apps.googleusercontent.com',
+    googleClientSecret:'K-RYn6-2RMMSgq4rGbqmDlH8'
 }
